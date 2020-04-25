a80ce5e03bf9858340a1310c50f161e0e4cb09af
Jiang Chao <840065231@qq.com>
2020-04-25 18:21:50 +0800

    Initial commit

---
+++ crawler/LuShiCrawler.py
@@ -0,0 +1,93 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import crawler.config as config
+import os
+import time
+
+
+def download_list(_url: str, _file_path):
+    """
+    使用递归方式获取列表，存储成文件
+    :param _page_num: 页码
+    :param _url: 开始页面url
+    :return:
+    """
+    reg1 = re.compile(r'^http(s){0,1}:\/\/[a-zA-z0-9.\/]+?\/page\/(\d+)')
+    match_res = reg1.search(_url)
+    _page_num = 1
+    if match_res:
+        _page_num = match_res.group(2)
+
+    _resp = requests.get(_url, headers=config.headers)
+    _soup = BeautifulSoup(_resp.text, 'lxml')
+
+    _deck_list_tag = _soup.select('table#deck-list')
+    if _deck_list_tag:
+        # <h4> list
+        _h4_list = _deck_list_tag[0].select("h4")
+        if _h4_list:
+            with open(_file_path, 'a', encoding='UTF-8') as _f:
+                _url_count = 0
+                for _h4_tag in _h4_list:
+                    _a_tag = _h4_tag.find("a")  # <a>
+                    if _a_tag:
+                        _href = _a_tag.get('href')
+                        _f.write(_href + '\n')
+                        _url_count += 1
+                _f.flush()
+                print('download [{}] page {} urls.'.format(_page_num, _url_count))
+    #  next page
+    _next_button = _soup.find("a", attrs={'class': 'page-link', 'rel': 'next'})
+
+    if _next_button:
+        _next_url = _next_button.get('href')
+        if _next_url:
+            download_list(_next_url, _file_path)
+
+
+def download_deck(_url, _file_path, _count):
+    _resp = requests.get(_url, headers=config.headers)
+    _soup = BeautifulSoup(_resp.text, 'lxml')
+
+    reg2 = re.compile(r'^http(s){0,1}:\/\/[a-zA-z0-9.\/]+?\/decks\/([a-zA-z\-0-9]+)')
+    reg_res = reg2.search(_url)
+    _deck_name = '无名卡组'
+    if reg_res:
+        _deck_name = reg_res.group(2)  # 卡组名字
+
+    # 卡组列表
+    card_frame_list = _soup.select('li.card-frame')
+    if card_frame_list:
+        with open(_file_path, 'a', encoding='UTF-8') as _f:
+            _f.write('[{}]\n'.format(_deck_name))
+            for card_frame in card_frame_list:
+                name_tag = card_frame.select('span.card-name')
+                cost_tag = card_frame.select('span.card-cost')
+                count_tag = card_frame.select('span.card-count')
+
+                card_name = name_tag[0].get_text()  # 卡名
+                card_cost = cost_tag[0].get_text()  # 卡费
+                card_count = count_tag[0].get_text()  # 数量
+                _f.write('\t{}, {}, {}\n'.format(card_name, card_cost, card_count))
+            _f.write('\n')
+            _f.flush()
+            print('下载第[{}]套卡组成功！'.format(_count))
+
+
+if __name__ == '__main__':
+    deck_url = "../resource/deck_url.txt"
+    _deck_frame_path = '../resource/deck_frame.txt'
+
+    if os.path.exists(deck_url):
+        os.remove(deck_url)
+    os.makedirs(os.path.dirname(deck_url), exist_ok=True)
+    download_list(config.start_url, deck_url)
+
+    # 遍历url下载所有卡组
+    os.makedirs(os.path.dirname(_deck_frame_path), exist_ok=True)
+    with open(deck_url, 'r') as f:
+        i = 0
+        for line in f:
+            i += 1
+            download_deck(line.strip(), _deck_frame_path, i)
---
+++ crawler/config.py
@@ -0,0 +1,20 @@
+headers = {
+    # ':authority': 'www.hearthstonetopdecks.com',
+    # ':method': 'GET',
+    # ':path': '/decks/nohandsgamers-quest-druid-masters-tour-los-angeles-top-8/',
+    # ':scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'cache-control': 'max-age=0',
+    'cookie': '__cfduid=d244561f837592ca289f755e182504f041587652285; _fbp=fb.1.1587652295420.2116948145; ac_user_id=act58cfutqmn20uddaba4888da508a7f236446a00652388a1e079a0b893b7c4cde343c95ffedaf2; __gads=ID=48ab0f8704468112:T=1587652306:S=ALNI_MbUSI8qzDgFFJb5Z6pcD9OkhVl8bA; _ga=GA1.2.710757217.1587652358; _gid=GA1.2.959078552.1587652358; __qca=P0-1725833405-1587652369539; __cf_bm=6dc30c33320e06a1b5b6e6cdf004ee70f08ab0f1-1587658932-1800-ActFfbzWvnaHB54/JjU1DN7r+XOKrppwjYJMhtpFrZirExXlzAdW492CHF80u7lTTXtPbTq35ohfc+s7yLLZJ7w=; _gat=1; GED_PLAYLIST_ACTIVITY=W3sidSI6IlFyTnAiLCJ0c2wiOjE1ODc2NTg5ODMsIm52IjoxLCJ1cHQiOjE1ODc2NTg5NzEsImx0IjoxNTg3NjU4OTgxfSx7InUiOiJjOFQrIiwidHNsIjoxNTg3NjU4OTgyLCJudiI6MCwidXB0IjoxNTg3NjU2MDkxLCJsdCI6MTU4NzY1ODIyN30seyJ1IjoiSDFMQyIsInRzbCI6MTU4NzY1ODk4MiwibnYiOjAsInVwdCI6MTU4NzY1Njg2NSwibHQiOjE1ODc2NTY5NzN9XQ..',
+    'if-modified-since': 'Thu, 23 Apr 2020 11:17:26 GMT',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36',
+}
+
+start_url = "https://www.hearthstonetopdecks.com/decks/page/575/"
